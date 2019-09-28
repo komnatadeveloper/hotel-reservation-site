2bd0aa4ff876fd14ab4e09db1f6a466681c8efd0
Emrah <ozyazgane@hotmail.com>
2019-09-28 07:27:34 +0300

    React Hotel Website Initial State is DONE

---
+++ src/components/StyledHero.js
@@ -0,0 +1,15 @@
+/* Related with styled components */
+
+
+import defaultImg from '../images/room-1.jpeg';
+import styled from 'styled-components';
+
+const StyledHero = styled.header`
+  min-height: 60vh;
+  background: url(${props => props.img ? props.img : defaultImg }) center/cover no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export default StyledHero;
